b7fa8880090d3bf91465185b974e59584add67e1
ketlyn-silvaa <ketlynsilva1609@gmail.com>
2026-09-08 01:09:51 -0300

    Update graph representation and print message

--- src/grafo.py
+++ src/grafo.py
@@ -1,11 +1,54 @@
-# Representação das entregas utilizando um grafo
+# Representação do mapa utilizando um grafo
 
 grafo = {
-    "Restaurante": ["Cliente 1", "Cliente 2"],
-    "Cliente 1": ["Cliente 3"],
-    "Cliente 2": ["Cliente 3"],
-    "Cliente 3": []
+    "Restaurante": {
+        "Centro": 2,
+        "Bela Vista": 3
+    },
+
+    "Centro": {
+        "Restaurante": 2,
+        "Liberdade": 2,
+        "Consolacao": 3
+    },
+
+    "Liberdade": {
+        "Centro": 2,
+        "Vila Mariana": 4
+    },
+
+    "Bela Vista": {
+        "Restaurante": 3,
+        "Vila Mariana": 3,
+        "Consolacao": 2
+    },
+
+    "Consolacao": {
+        "Centro": 3,
+        "Bela Vista": 2,
+        "Pinheiros": 5
+    },
+
+    "Vila Mariana": {
+        "Liberdade": 4,
+        "Bela Vista": 3,
+        "Moema": 4
+    },
+
+    "Moema": {
+        "Vila Mariana": 4,
+        "Pinheiros": 6
+    },
+
+    "Pinheiros": {
+        "Consolacao": 5,
+        "Moema": 6,
+        "Perdizes": 4
+    },
+
+    "Perdizes": {
+        "Pinheiros": 4
+    }
 }
 
-print("Grafo de rotas criado com sucesso!")
-print(grafo)
+print("🗺️ Grafo de rotas criado com sucesso!")
